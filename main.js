ac228a932963b7dd477779916b52204821e9cee7
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-07-14 18:42:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var capture;
 var status = '';
 var input = "";
+objects = [];
 
 function setup() { 
     canvas = createCanvas(300, 300); 
@@ -16,6 +17,33 @@ function draw() {
     scale(-1, 1);
     image(capture, 0, 0, 320, 300);
     pop();
+
+
+    if(status != "") {
+        objectDetector.detect(capture, gotResults);
+
+                for(i=0; i < objects.length; i++) {
+
+                    if (objects[i].label == input) {
+
+                    document.getElementById("status").innerHTML = 'Status: Object Detected';
+                    document.getElementById('ObjectNo').innerHTML = "No. of Objects: " + objects.length
+        
+                    fill("#FFFFFF");
+                    percent = floor(objects[i].confidence * 100);
+                    text(objects[i].label + " " + percent +"%", objects[i].x + 15, objects[i].y + 15);
+                    noFill();
+                    stroke("#FFFFFF");
+                    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].width);
+
+                }
+            
+            }
+            
+        
+    }
+
+    
   }
 
 function start() {
@@ -27,8 +55,17 @@ function start() {
 function modelLoaded() {
     console.log('model loaded');
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+
+}
+
+function gotResults(error, results) {
+
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
 
 }
